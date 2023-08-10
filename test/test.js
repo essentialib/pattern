53f6f897d4a6e3e13cf7bb62e91750695bce4505
Hyunseung Ryu <56152093+rhseung@users.noreply.github.com>
2023-08-11 08:26:43 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,15 @@ const Pattern = require('../src/index.js');
 
 // /^(?<timestamp>\d+),(?<author>.+)$/
 const regex = Pattern(_ => _
-        .find(_ => _.find('abc'))
-    ,)
+    .startOfLine()
+    .capture('timestamp', timestamp => timestamp
+        .digit().oneOrMore()
+    )
+    .then(',')
+    .capture('author', author => author
+        .anything().oneOrMore()
+    )
+    .endOfLine()
+)
 
 console.log(regex.toRegExp())
